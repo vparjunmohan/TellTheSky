aa3cf33e1a0771c62a225ff11f56ab1cec279875
Arjun Mohan <62532677+vparjunmohan@users.noreply.github.com>
2022-11-25 18:11:41 +0530

    api call for current location

--- Tell The Sky/Tell The Sky/ViewController.swift
+++ Tell The Sky/Tell The Sky/ViewController.swift
@@ -7,10 +7,12 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
 
 class ViewController: UIViewController {
     
     var locationManager = CLLocationManager()
+    let key = "60054c7b80e9b5dbf30d5b02ec6663e8"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +41,29 @@ extension ViewController: CLLocationManagerDelegate{
             currentLoc = locationManager.location
             print(currentLoc.coordinate.latitude)
             print(currentLoc.coordinate.longitude)
+            
+//            let url = " https://api.openweathermap.org/data/3.0/onecall?lat=\(currentLoc.coordinate.latitude)&lon=\(currentLoc.coordinate.longitude)&appid=\(key)"
+            
+            let url = "https://api.openweathermap.org/data/2.5/weather?lat=13.07&lon=77.59&appid=60054c7b80e9b5dbf30d5b02ec6663e8"
+            
+            
+            AF.request(url).responseJSON(completionHandler: { [self] response in
+                switch response.result {
+                case .success:
+                    print("success")
+                    if let responseValue = response.value as? [String: Any]{
+                        print(responseValue)
+                    }
+                    break
+                default:
+                    break
+                }
+                
+            } )
+            
+
+            
+//        https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
         case .notDetermined:
             print("not determined")
         case .restricted:
