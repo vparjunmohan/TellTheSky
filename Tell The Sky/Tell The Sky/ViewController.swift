24a72b6a92dc5aa8331c1a0c127334d5020d89b7
Arjun Mohan <62532677+vparjunmohan@users.noreply.github.com>
2022-11-25 16:50:06 +0530

    fetching user's current location

--- Tell The Sky/Tell The Sky/ViewController.swift
+++ Tell The Sky/Tell The Sky/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+
     }
-
-
+    
+    
+    
 }
 
+extension ViewController: CLLocationManagerDelegate{
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        var currentLoc: CLLocation!
+        let authorizationStatus: CLAuthorizationStatus
+        if #available(iOS 14, *) {
+            authorizationStatus = locationManager.authorizationStatus
+        } else {
+            authorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        switch authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            currentLoc = locationManager.location
+            print(currentLoc.coordinate.latitude)
+            print(currentLoc.coordinate.longitude)
+        case .notDetermined:
+            print("not determined")
+        case .restricted:
+            print("restricted")
+        case .denied:
+            // handle location access if it is denied
+            print("denied")
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+}
